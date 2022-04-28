485ace27e55e52e84a62e1fb6a286c238b4a3de9
Ruta Gumuliauskaite <rgumuli@corp.transunion.com>
2022-04-28 09:41:26 +0300

    Add project files.

---
+++ src/CalculatorProject/Calculations.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CalculationLibraries
+{
+    public class Calculations
+    {
+        public int Sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ tests/CalculatorProject.Tests/CalculationsTests.cs
@@ -0,0 +1,21 @@
+﻿using CalculationLibraries;
+using NUnit.Framework;
+using FluentAssertions;
+
+namespace CalculatorProject.Tests
+{
+    public class CalculationsTests
+    {
+        [TestCase(2, 2, 4)]
+        [TestCase(1, 1, 2)]
+        [TestCase(3, 5, 8)]
+        public void ReturnSumResult(int a, int b, int expected)
+        {
+            Calculations calculations = new Calculations();
+
+            int actual = calculations.Sum(a, b);
+
+            actual.Should().Be(expected);
+        }
+    }
+}
